e0e772c3ec755babaff0530741a53606fbd78758
Minh <kckc253261@gmail.com>
2024-05-14 08:21:32 +0700

    style(DAL): DAL_KhuyenMai

--- DAL/DAL_KhuyenMai.cs
+++ DAL/DAL_KhuyenMai.cs
@@ -9,66 +9,26 @@ namespace DAL
 {
     public class DAL_KhuyenMai
     {
+        ShopDatabaseEntities db = new ShopDatabaseEntities();
         public void Them(tblKhuyenMai KhuyenMai)
         {
-            try
-            {
-                using (ShopDatabaseEntities db = new ShopDatabaseEntities())
-                {
-                    db.tblKhuyenMais.Add(KhuyenMai);
-                    db.SaveChanges();
-                }
-            }
-            catch (Exception ex) { Console.WriteLine(ex.Message); }
+            db.tblKhuyenMais.Add(KhuyenMai);
+            db.SaveChanges();
         }
         public void Sua(tblKhuyenMai OldKhuyenMai, tblKhuyenMai NewKhuyenMai)
         {
-            try
-            {
-                using (ShopDatabaseEntities db = new ShopDatabaseEntities())
-                {
-                    tblKhuyenMai KhuyenMai = db.tblKhuyenMais.ToList().Find(x => Equals(x.MaKM, OldKhuyenMai.MaKM));
-                    KhuyenMai.TenKM = NewKhuyenMai.TenKM;
-                    KhuyenMai.NgayBatDau = NewKhuyenMai.NgayBatDau;
-                    KhuyenMai.NgayKetThuc = NewKhuyenMai.NgayKetThuc;
-                    KhuyenMai.PhamTramGiam = NewKhuyenMai.PhamTramGiam.Value;
-                    db.SaveChanges();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            tblKhuyenMai KhuyenMai = db.tblKhuyenMais.ToList().Find(x => Equals(x.MaKM, OldKhuyenMai.MaKM));
+            KhuyenMai.TenKM = NewKhuyenMai.TenKM;
+            KhuyenMai.NgayBatDau = NewKhuyenMai.NgayBatDau;
+            KhuyenMai.NgayKetThuc = NewKhuyenMai.NgayKetThuc;
+            KhuyenMai.PhamTramGiam = NewKhuyenMai.PhamTramGiam.Value;
+            db.SaveChanges();
         }
         public void Xoa(tblKhuyenMai KhuyenMai)
         {
-            try
-            {
-                using (ShopDatabaseEntities db = new ShopDatabaseEntities())
-                {
-                    db.tblKhuyenMais.Remove(KhuyenMai);
-                    db.SaveChanges();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            db.tblKhuyenMais.Remove(KhuyenMai);
+            db.SaveChanges();
         }
-        public List<tblKhuyenMai> DanhSachKhuyenMai()
-        {
-            try
-            {
-                using (ShopDatabaseEntities db = new ShopDatabaseEntities())
-                {
-                    return db.tblKhuyenMais.ToList();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return new List<tblKhuyenMai>();
-            }
-        }
+        public List<tblKhuyenMai> DanhSachKhuyenMai() => db.tblKhuyenMais.ToList();
     }
 }
