cebc4d54ebf7cb6fa993f8c7073b7a7646a7ed85
Minh <kckc253261@gmail.com>
2024-05-16 18:48:06 +0700

    chore(voucher): add unit entity to enable  value comparison

---
+++ BUS/BUS_DonVi.cs
@@ -0,0 +1,16 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class BUS_DonVi
+    {
+        DAL_DonVi DAL_DonVi = new DAL_DonVi();
+        public List<tblDonVi> DS_DonVi() => DAL_DonVi.DS_DonVi();
+    }
+}
