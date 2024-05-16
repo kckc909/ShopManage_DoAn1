cd03be1c2215ddf460d679b2d7c8280ab50d04b7
Minh <kckc253261@gmail.com>
2024-05-17 01:09:57 +0700

    feat(tools): check email and phone numbers

---
+++ BUS/Tools/Check.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web.UI;
+
+namespace GUI.Tools
+{
+    public static class Check
+    {
+        public static bool IsValidEmail(string email)
+        {
+            if (!email.Trim().Equals(""))
+            {
+                string pattern = @"^(?!\-)(?![0-9])[a-zA-Z0-9\-]{1,63}(\.[a-zA-Z]{2,})+$";
+                Regex regex = new Regex(pattern);
+                return regex.IsMatch(email);
+            }
+            return false;
+        }
+
+        public static bool IsValidPhoneNumber(string pn)
+        {
+            if (pn.Length < 10 || pn.Length > 11)
+            {
+                return false;
+            }
+            string pattern = @"^\+?(\d[\d-. ]+)?(\([\d-. ]+\))?[\d-. ]+\d$";
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(pn);
+        }
+    }
+}
