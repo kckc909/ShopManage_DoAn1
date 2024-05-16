da3f563875441296f07692da471123f1136f46f4
Minh <kckc253261@gmail.com>
2024-05-16 18:29:38 +0700

    fix(dal): properly dispose database connections

--- DAL/DAL_DonVi.cs
+++ DAL/DAL_DonVi.cs
@@ -10,6 +10,14 @@ namespace DAL
     public class DAL_DonVi
     {
         ShopDatabaseEntities db = new ShopDatabaseEntities();
+        public void Them(tblDonVi DonVi)
+        {
+            if (DonVi != null && DS_DonVi().Exists(x => x.DonVi.Equals(DonVi.DonVi)))
+            {
+                db.tblDonVis.Add(DonVi);
+                db.SaveChanges();
+            }
+        }
         public List<tblDonVi> DS_DonVi() => db.tblDonVis.ToList();
     }
 }
--- DAL/DAL_MatHang.cs
+++ DAL/DAL_MatHang.cs
@@ -10,60 +10,41 @@ namespace DAL
 {
     public class DAL_MatHang
     {
+        ShopDatabaseEntities db = new ShopDatabaseEntities();
         public void Them(tblMatHang MatHang)
         {
-            using (ShopDatabaseEntities db = new ShopDatabaseEntities())
-            {
-                db.tblMatHangs.Add(MatHang);
-                db.SaveChanges();
-            }
+            db.tblMatHangs.Add(MatHang);
+            db.SaveChanges();
         }
         
         public void Sua(tblMatHang OldMatHang, tblMatHang NewMatHang)
         {
-            using (ShopDatabaseEntities db = new ShopDatabaseEntities())
-            {
-                tblMatHang MatHang = db.tblMatHangs.ToList().Find(x => Equals(x.MaMH, OldMatHang.MaMH));
+            tblMatHang MatHang = db.tblMatHangs.ToList().Find(x => Equals(x.MaMH, OldMatHang.MaMH));
 
-                MatHang.TenMH = NewMatHang.TenMH;
-                MatHang.Mota = NewMatHang.Mota;
-                MatHang.LinkHinhAnh = NewMatHang.LinkHinhAnh;
-                MatHang.GiaBan = NewMatHang.GiaBan;
-                MatHang.SoLuong = NewMatHang.SoLuong;
-                MatHang.DonViTinh = NewMatHang.DonViTinh;
-                MatHang.HanSuDung = NewMatHang.HanSuDung;
-                MatHang.MaKM = NewMatHang.MaKM;
-                MatHang.MaLoai = NewMatHang.MaLoai;
+            MatHang.TenMH = NewMatHang.TenMH;
+            MatHang.Mota = NewMatHang.Mota;
+            MatHang.LinkHinhAnh = NewMatHang.LinkHinhAnh;
+            MatHang.GiaBan = NewMatHang.GiaBan;
+            MatHang.SoLuong = NewMatHang.SoLuong;
+            MatHang.DonViTinh = NewMatHang.DonViTinh;
+            MatHang.HanSuDung = NewMatHang.HanSuDung;
+            MatHang.MaKM = NewMatHang.MaKM;
+            MatHang.MaLoai = NewMatHang.MaLoai;
 
-                db.SaveChanges();
-            }
+            db.SaveChanges();
         }
         
         public void Xoa(tblMatHang MatHang)
         {
-            using (ShopDatabaseEntities db = new ShopDatabaseEntities())
-            {
-                db.tblMatHangs.Remove(db.tblMatHangs.Find(MatHang.MaMH));
-                db.SaveChanges();
-            }
+            db.tblMatHangs.Remove(db.tblMatHangs.Find(MatHang.MaMH));
+            db.SaveChanges();
         }
         
-        public List<tblMatHang> DanhSachMatHang()
-        {
-            using (ShopDatabaseEntities db = new ShopDatabaseEntities())
-            {
-                return db.tblMatHangs.ToList();
-            }
-        }
+        public List<tblMatHang> DanhSachMatHang() => db.tblMatHangs.ToList();
 
         public void TaoHinhAnh(string SourceImagePath,string ImageName)
         {
             File.Copy(SourceImagePath, ImageName, false);
-        }
-
-        public string LayPathHinhAnh(string ImageName)
-        {
-            return Path.Combine(MyDefault.Path_AnhMatHang, ImageName);
         }
     }
 }
